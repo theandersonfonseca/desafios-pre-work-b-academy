b8278fb3a64bc9762f611b67c8af0c5c6094dc90
Anderson <andersonfonseca1999@hotmail.com>
2021-08-17 16:41:44 -0300

    Add feature to toggle app div visibility

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,19 @@
 import "./style.css";
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const app = document.querySelector('[data-js="app"]');
+const link = document.querySelector('[data-js="toggle-visibility"]');
+
+app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+function toggleVisibility() {
+  app.classList.toggle("app__hidden");
+
+  const linkText = app.classList.contains("app__hidden") ? "Show" : "Hide";
+
+  link.innerHTML = linkText;
+}
+
+link.addEventListener("click", toggleVisibility);
