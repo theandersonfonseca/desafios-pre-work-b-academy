927a47bb642b28d0511512c6c0a1bd83baf9dbe9
Anderson <andersonfonseca1999@hotmail.com>
2021-08-20 13:03:41 -0300

    Add fetch to cars

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,49 @@
-import './style.css'
+import "./style.css";
+
+const URL = "http://localhost:3333/cars";
+
+const tableBody = document.querySelector('[data-js="table-body"]');
+const tableWarning = document.querySelector('[data-js="table-warning"]');
+
+function createDataForTable(values) {
+  const tr = document.createElement("tr");
+
+  values.forEach((value) => {
+    const td = document.createElement("td");
+    td.innerHTML = value;
+    tr.appendChild(td);
+  });
+
+  return tr;
+}
+
+function addCarsToTable(cars) {
+  tableWarning.parentElement.style.display = "none";
+
+  cars.forEach((car) => {
+    tableBody.appendChild(createDataForTable(Object.values(car)));
+  });
+}
+
+async function getCars() {
+  let data;
+
+  try {
+    const response = await fetch(URL);
+    data = await response.json();
+  } catch (err) {
+    tableWarning.innerHTML = "Não foi possível receber os dados";
+
+    return;
+  }
+
+  if (data.length === 0) {
+    tableWarning.innerHTML = "Nenhum carro encontrado";
+
+    return;
+  }
+
+  addCarsToTable(data);
+}
+
+getCars();
