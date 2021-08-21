78b4345dce37d3f57254b7668f405ca64c334087
Anderson <andersonfonseca1999@hotmail.com>
2021-08-21 13:13:37 -0300

    Add typing to main.ts and http.ts

--- dia5/app/src/main.ts
+++ dia5/app/src/main.ts
@@ -1,8 +1,175 @@
-import './style.css'
+import "./style.css";
+import { get, post, del } from "./http";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const url = "http://localhost:3333/cars";
+const form = document.querySelector('[data-js="cars-form"]');
+const table = document.querySelector('[data-js="table"]');
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const getFormElement = (event: Event) => (elementName: string) => {
+  const target = event.target as HTMLFormElement;
+
+  const element = (target.elements as { [key: string]: any })[
+    elementName
+  ] as HTMLInputElement;
+
+  return element;
+};
+
+const elementTypes = {
+  image: createImage,
+  text: createText,
+  color: createColor,
+};
+
+type imageProps = {
+  src: string;
+  alt: string;
+};
+
+function createImage(data: imageProps) {
+  const td = document.createElement("td");
+  const img = document.createElement("img");
+  img.src = data.src;
+  img.alt = data.alt;
+  img.width = 100;
+  td.appendChild(img);
+  return td;
+}
+
+function createText(value: string) {
+  const td = document.createElement("td");
+  td.textContent = value;
+  return td;
+}
+
+function createColor(value: string) {
+  const td = document.createElement("td");
+  const div = document.createElement("div");
+  div.style.width = "100px";
+  div.style.height = "100px";
+  div.style.background = value;
+  td.appendChild(div);
+  return td;
+}
+
+form?.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const getElement = getFormElement(e);
+
+  const data = {
+    image: getElement("image").value,
+    brandModel: getElement("brand-model").value,
+    year: getElement("year").value,
+    plate: getElement("plate").value,
+    color: getElement("color").value,
+  };
+
+  const result = await post(url, data);
+
+  if (result.error) {
+    console.log("deu erro na hora de cadastrar", result.message);
+    return;
+  }
+
+  const noContent = document.querySelector('[data-js="no-content"]');
+  if (noContent) {
+    table?.removeChild(noContent);
+  }
+
+  createTableRow(data);
+
+  const target = e.target as HTMLFormElement;
+
+  target.reset();
+  getElement("image").focus();
+});
+
+type carProps = {
+  image: string;
+  brandModel: string;
+  year: string;
+  plate: string;
+  color: string;
+};
+
+function createTableRow(data: carProps) {
+  const elements = [
+    { type: "image", value: { src: data.image, alt: data.brandModel } },
+    { type: "text", value: data.brandModel },
+    { type: "text", value: data.year },
+    { type: "text", value: data.plate },
+    { type: "color", value: data.color },
+  ];
+
+  const tr = document.createElement("tr");
+  tr.dataset.plate = data.plate;
+
+  elements.forEach((element) => {
+    const td = (elementTypes as { [key: string]: any })[element.type](
+      element.value
+    );
+
+    tr.appendChild(td);
+  });
+
+  const button = document.createElement("button");
+  button.textContent = "Excluir";
+  button.dataset.plate = data.plate;
+
+  button.addEventListener("click", handleDelete);
+
+  tr.appendChild(button);
+
+  table?.appendChild(tr);
+}
+
+async function handleDelete(e: { target: any }) {
+  const button = e.target;
+  const plate = button.dataset.plate;
+
+  const result = await del(url, { plate });
+
+  if (result.error) {
+    console.log("erro ao deletar", result.message);
+    return;
+  }
+
+  const tr = document.querySelector(`tr[data-plate="${plate}"]`);
+  table?.removeChild(tr!);
+  button.removeEventListener("click", handleDelete);
+
+  const allTrs = table?.querySelector("tr");
+  if (!allTrs) {
+    createNoCarRow();
+  }
+}
+
+function createNoCarRow() {
+  const tr = document.createElement("tr");
+  const td = document.createElement("td");
+  const thsLength = document.querySelectorAll("table th").length;
+  td.setAttribute("colspan", thsLength.toString());
+  td.textContent = "Nenhum carro encontrado";
+
+  tr.dataset.js = "no-content";
+  tr.appendChild(td);
+  table?.appendChild(tr);
+}
+
+async function main() {
+  const result = await get(url);
+
+  if (result.error) {
+    console.log("Erro ao buscar carros", result.message);
+    return;
+  }
+
+  if (result.length === 0) {
+    createNoCarRow();
+    return;
+  }
+
+  result.forEach(createTableRow);
+}
+
+main();
